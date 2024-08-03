b0a43aef489aec2b1e885e3fea1b678789d49adb
Dannhawe <lucky.nguyenvuduc@gmail.com>
2024-08-03 14:46:03 +0700

    Initial repo

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+"use client";
+import "./globals.css";
+import "./data-tables-css.css";
+import "./satoshi.css";
+import { useState, useEffect } from "react";
+import Loader from "@/components/common/Loader";
+
+import Sidebar from "@/components/Sidebar/Sidebar";
+import Header from "@/components/Header";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return (
+    <html lang="en">
+      <body suppressHydrationWarning={true}>
+        <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
